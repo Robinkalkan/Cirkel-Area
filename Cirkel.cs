806a63e60c4c93001123c3970c424760de463a95
Robinkalkan <Robin.kalkan@chasacademy.se>
2023-10-18 16:30:44 +0200

    Add project files.

---
+++ Cirkel.cs
@@ -0,0 +1,14 @@
+﻿class Circle
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class program
+{
+    static void Main()
+    {
+        var circle1 = new Circle(5);
+        var circle2 = new Circle(6);
+
+        Console.WriteLine($"Arean på den första cirkeln med radie 5 är: {circle1.GetArea()}");
+        Console.WriteLine($"Arean på den andra cirkeln med radie 6 är: {circle2.GetArea()}");
+    }
+}
